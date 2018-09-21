5171b590931e6bec88417301b53086630b9059c3
Animesh Kumar <animesh.ultimate@gmail.com>
2018-09-21 13:29:44 +0530

    Modified

--- cms-plugin/cms-plugin.php
+++ cms-plugin/cms-plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: CMS for minimum wages
 Plugin URI: 
-Description: Showing the notifications based on input date2
+Description: Showing the notifications based on input date
 Version: 1.0.0
 Author: Animesh
 Author URI: 
